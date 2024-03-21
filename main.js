b914df219f53e8b546fc193dd3eac744fa287e74
Jamie Lee Rogers <156160941+JLRcodes@users.noreply.github.com>
2024-03-21 11:24:17 +1100

    Add numbers functionality to main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Store all components on HTML in our JS
+  let clear = document.querySelector("#clear-btn");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operators");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", function (e) {
+      handleNumber(e.target.textContent);
+      currentScreen.textContent = currentValue;
+    })
+  );
+});
+function handleNumber(num) {
+  if (currentValue.length <= 5) {
+    currentValue += num;
+  }
+}
